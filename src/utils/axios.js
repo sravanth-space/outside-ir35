2365fd0efd6f998f7feac938d711e6e7e4bb6cd2
Sravanth <sravanthbaratam@gmail.com>
2023-07-18 07:26:15 +0100

    Feat/v0.1 (#33)
    * feat(ts): Upgraded to use ts.
    
    Added types
    
    Updated the codebase accdly.
    
    * fix: API url.

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import { clearStore } from "../features/user/userSlice";
 import { getUserFromLocalStorage } from "./localStorage";
 
 const customFetch = axios.create({
-  baseURL: "https://oir35-api.onrender.com/api/v1/",
+  baseURL: "http://18.133.248.188:5000/api/v1/",
 });
 
 customFetch.interceptors.request.use(config => {
